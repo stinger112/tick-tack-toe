cb59cf1aa3a0d8383d5c6e1e3dc99a5d689bd461
stinger112 <stinger112@gmail.com>
2015-10-19 22:04:28 +0700

    Separate code

---
+++ database.js
@@ -0,0 +1,79 @@
+'use strict';
+
+var bb = require('bluebird');
+var sqlite3 = require('sqlite3').verbose();
+
+var db = new sqlite3.Database(':memory:');
+db.run("CREATE TABLE Moves (PlayerType TEXT, xPos INTEGER, yPos INTEGER)");
+
+var database = {
+  checkWinner: function(move, callback) {
+    var lengthCondition = 5; // Condition for win
+
+    db.serialize(function() {
+      var parameters = [move.type, move.position.x, move.position.y];
+      db.run("INSERT INTO Moves VALUES(?, ?, ?)", parameters);
+
+      /** Select signs in the nearest 10 cells by x coordinate in ASC order **/
+      var xPosQuery = "SELECT xPos FROM Moves WHERE"
+        + " PlayerType='" + move.type + "'"
+        + " AND yPos = " + move.position.y
+        + " AND xPos > " + (move.position.x - lengthCondition)
+        + " AND xPos < " + (move.position.x + lengthCondition)
+        + " ORDER BY xPos";
+
+      var tmp = move.position.x; // Hold current x position for comparison with the next captured position
+      var lineCounter = 1; // Counter for natural order signs (always starts from 1)
+
+      db.each(xPosQuery, function(err, row) {
+        //console.log(row.xPos);
+
+        // Check that current sign don't broke natural order
+        if (row.xPos === tmp + 1)
+          lineCounter++;
+        else
+          lineCounter = 1;
+
+        tmp = row.xPos;
+
+        // We found 5 signs in natural order! Win!
+        if (lineCounter === 5) {
+          callback();
+        }
+      });
+
+      /** Select signs in the nearest 10 cells by y coordinate in ASC order **/
+      var yPosQuery = "SELECT yPos FROM Moves WHERE"
+        + " PlayerType='" + move.type + "'"
+        + " AND xPos = " + move.position.x
+        + " AND yPos > " + (move.position.y - lengthCondition)
+        + " AND yPos < " + (move.position.y + lengthCondition)
+        + " ORDER BY yPos";
+
+      tmp = move.position.y; // Hold current y position for comparison with the next captured position
+      lineCounter = 1; // Counter for natural order signs (always starts from 1)
+
+      db.each(yPosQuery, function(err, row) {
+        // Check that current sign don't broke natural order
+        if (row.yPos === tmp + 1)
+          lineCounter++;
+        else
+          lineCounter = 1;
+
+        tmp = row.yPos;
+
+        // We found 5 signs in natural order! Win!
+        if (lineCounter === 5) {
+          callback();
+        }
+      });
+
+    });
+  },
+
+  clear: function () {
+    db.close();
+  }
+};
+
+module.exports = database;
